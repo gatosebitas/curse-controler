ab504116fd30d23102e4f9eeb3dd9a6d75ff8848
yoelZalas <yoel.zalas@gmail.com>
2017-01-21 20:06:44 -0500

    remove STATICFILES_DIRS

--- sociallogin/settings.py
+++ sociallogin/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
